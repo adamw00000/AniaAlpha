6dec6e22454b74c265bbba0a73d0b06d3f22435b
adamw00000 <adam.wawrzenczyk@gmail.com>
2019-02-23 09:58:43 +0100

    Aliases upgraded

--- AniaAlpha/Modules/AliasModule.cs
+++ AniaAlpha/Modules/AliasModule.cs
@@ -25,27 +25,48 @@ namespace AniaAlpha.Modules
         [Command("Add", RunMode = RunMode.Async)]
         public async Task Add(string animeName, string alias)
         {
-            if (_context.Aliases.Any(x => x.Name == alias))
+            long animeId;
+
+            if (Uri.TryCreate(animeName, UriKind.Absolute, out Uri resultUri))
             {
-                await ReplyAsync("Alias already exists!");
-                return;
+                if ((resultUri.Scheme == "http" || resultUri.Scheme == "https") && resultUri.Host == "myanimelist.net"
+                    && resultUri.Segments.Length >= 3 && resultUri.Segments[0] == "/" && resultUri.Segments[1] == "anime/"
+                    && long.TryParse(resultUri.Segments[2].Remove(resultUri.Segments[2].Length - 1, 1), out animeId))
+                {
+                    
+                }
+                else
+                {
+                    await ReplyAsync("No anime matching!");
+                    return;
+                }
+            }
+            else
+            {
+                if (_context.Aliases.Any(x => x.Name == alias))
+                {
+                    await ReplyAsync("Alias already exists!");
+                    return;
+                }
+
+                var searchResult = (await _jikan.SearchAnime(animeName)).Results.Take(5).ToList();
+                if (searchResult.Count == 0)
+                {
+                    await ReplyAsync("No anime matching!");
+                    return;
+                }
+
+                var perfectMatch = searchResult.FirstOrDefault(result => result.Title == animeName);
+                if (perfectMatch == null)
+                {
+                    await ReplyAsync("No anime matching!");
+                    return;
+                }
+
+                animeId = perfectMatch.MalId;
             }
 
-            var searchResult = (await _jikan.SearchAnime(animeName)).Results.Take(5).ToList();
-            if (searchResult.Count == 0)
-            {
-                await ReplyAsync("No anime matching!");
-                return;
-            }
-
-            var perfectMatch = searchResult.FirstOrDefault(result => result.Title == animeName);
-            if (perfectMatch == null)
-            {
-                await ReplyAsync("No anime matching!");
-                return;
-            }
-
-            var animeAlias = new AnimeAlias { AnimeId = perfectMatch.MalId, Name = alias };
+            var animeAlias = new AnimeAlias { AnimeId = animeId, Name = alias };
             await _context.Aliases.AddAsync(animeAlias);
 
             var modified = await _context.SaveChangesAsync();
@@ -62,6 +83,16 @@ namespace AniaAlpha.Modules
         [Command("Add", RunMode = RunMode.Async)]
         public async Task Add(params string[] parameters)
         {
+            if (parameters.Length == 2)
+            {
+                if (parameters[1].StartsWith('{') && parameters[1].EndsWith('}'))
+                {
+                    parameters[1].Remove(parameters[1].Length - 1, 1).Remove(0, 1);
+                }
+                await Add(parameters[0], parameters[1]);
+                return;
+            }
+
             if (!parameters[parameters.Length - 1].EndsWith('}'))
             {
                 await ReplyAsync("Parsing error. Did you forget about {alias}?");
