1e6a41424ab84857058ec8dbe6cf401d7ccefdf5
adamw00000 <adam.wawrzenczyk@gmail.com>
2019-04-22 22:48:22 +0200

    Download links are correct

--- AniaAlpha/Modules/HorribleSubsModule.cs
+++ AniaAlpha/Modules/HorribleSubsModule.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.Commands;
 using HtmlAgilityPack;
+using PuppeteerSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -95,14 +96,70 @@ namespace AniaAlpha.Modules
             }
             var trackedShow = trackedShowList.First();
 
-            var dlDoc = await web.LoadFromWebAsync(trackedShow.Url);
-            var nodes = doc.DocumentNode
-                .SelectNodes("//*[contains(@class, 'dl-type hs-torrent-link')]/a");
-            var dlLinks = doc.DocumentNode
-                .SelectNodes("//*[contains(@class, 'dl-type hs-torrent-link')]/a")
-                .Select(node => node.Attributes["href"].Value);
+            var responseString = await GetNextDl(trackedShow.Url);
+            var responseLines = responseString.Split('\n').AsEnumerable();
 
-            string response = dlLinks.Aggregate("**Download links:**", (prev, next) => prev + $"{next}\n");
+            int i = 0;
+            const int messageSize = 10;
+            string aggregateBase = "**Download links:**\n";
+            while (true)
+            {
+                if (!responseLines.Any())
+                    return;
+
+                string response = responseLines.Take(messageSize)
+                    .Aggregate(aggregateBase, (prev, next) => prev + $"{next}\n");
+                await ReplyAsync(response);
+
+                aggregateBase = "";
+                i++;
+                responseLines = responseLines.Skip(messageSize);
+            }
+
+            //var dlDoc = await web.LoadFromWebAsync(trackedShow.Url);
+            //var nodes = doc.DocumentNode
+            //    .SelectNodes("//*[contains(@class, 'dl-type hs-torrent-link')]/a");
+            //var dlLinks = doc.DocumentNode
+            //    .SelectNodes("//*[contains(@class, 'dl-type hs-torrent-link')]/a")
+            //    .Select(node => node.Attributes["href"].Value);
+
+            //string response = dlLinks.Aggregate("**Download links:**", (prev, next) => prev + $"{next}\n");
+        }
+
+        public async Task<string> GetNextDl(string url)
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(url);
+
+            int i = 0;
+            while (true)
+            {
+                var moreButton = await page.XPathAsync($"//*[contains(@class, 'more-button') and @id=\"{i}\"]");
+                if (!moreButton.Any())
+                    break;
+                await moreButton.FirstOrDefault()?.ClickAsync();
+                i++;
+            }
+
+            var matches = await page.XPathAsync($"//*[contains(@class, 'rls-label')]");
+            matches.ToList()
+                .ForEach(async match => await match.ClickAsync());
+
+            var linkSpans = await page
+                .XPathAsync($"//*[contains(@class, 'rls-link link-720p')]/span[contains(@class, 'dl-type hs-torrent-link')]/a");
+
+            List<string> links = new List<string>();
+            foreach(var span in linkSpans)
+            {
+                string link = await (await span.GetPropertyAsync("href")).JsonValueAsync<string>();
+                links.Add(link);
+            }
+            return links.Aggregate("", (prev, next) => prev + $"{next}\n");
         }
 
         public class HsAnime
